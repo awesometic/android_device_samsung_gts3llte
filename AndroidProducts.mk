cfebd692bfce69ef3e00868743ad3dbee57fe274
Deokgyu Yang <secugyu@gmail.com>
2021-10-29 18:53:03 +0900

    gts3llte: Edit properties for HavocOS
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gts3llte.mk
+    $(LOCAL_DIR)/havoc_gts3llte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gts3llte-userdebug
+    havoc_gts3llte-userdebug
