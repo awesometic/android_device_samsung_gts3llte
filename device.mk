5e5bbbbc89300c0b4682f49001c87f083180d5b4
Deokgyu Yang <secugyu@gmail.com>
2022-10-08 11:30:13 +0900

    gts3llte: device: Remove dead targets
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: Ie1e4fa0504c58621e5956d16d845a69ff69c258c

--- device.mk
+++ device.mk
@@ -43,11 +43,8 @@ PRODUCT_COPY_FILES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
-    android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2 \
-    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0 \
-    android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2 \
     librmnetctl \
     libxml2 \
